8a883a82d2608886cef4d509a41f2c95ceab1115
Bruno Adami <brunoadamiserine@usp.br>
2020-12-29 15:58:30 -0300

    change page

--- src/containers/spoint.js
+++ src/containers/spoint.js
@@ -14,7 +14,7 @@ const Spoint = () => {
   // <<<<<<<<<<<<<< STATE VARIABLES >>>>>>>>>>>>>>>
 
   // Page
-  const [page, setPage] = useState('test');
+  const [page, setPage] = useState('home');
   const [subPage, setSubPage] = useState('home');
   // General User
   const [userInfo, setUserInfo] = useState({
